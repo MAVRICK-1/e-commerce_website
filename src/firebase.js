f87fda14e3732a601dd11d42b950eb621c499f1b
Rishi Mondal <146999057+MAVRICK-1@users.noreply.github.com>
2024-02-04 06:01:59 +0000

    Added Environmental variables

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,14 @@ import { initializeApp } from "firebase/app"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCLbDQfukIhsCxal07QN3bDHMdXzko1CZ8",
-  authDomain: "mavrick-1a92d.firebaseapp.com",
-  projectId: "mavrick-1a92d",
-  storageBucket: "mavrick-1a92d.appspot.com",
-  messagingSenderId: "588641997057",
-  appId: "1:588641997057:web:02335d56c00b06961ccb00",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  //databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 
 };
 // Initialize Firebase
