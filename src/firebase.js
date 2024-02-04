b661009d8b6dd10fac350fd43f0b0522db7271ee
Rishi Mondal <146999057+MAVRICK-1@users.noreply.github.com>
2024-02-04 06:11:37 +0000

    minor change

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,13 @@ import { initializeApp } from "firebase/app"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCLbDQfukIhsCxal07QN3bDHMdXzko1CZ8",
+  authDomain: "mavrick-1a92d.firebaseapp.com",
+  projectId: "mavrick-1a92d",
+  storageBucket: "mavrick-1a92d.appspot.com",
+  messagingSenderId: "588641997057",
+  appId: "1:588641997057:web:02335d56c00b06961ccb00",
+  measurementId: "G-TFKR3TBE3Y"
 
 };
 // Initialize Firebase
