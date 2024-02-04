6dbbc1022afd21794ec1579853fa670c02f9ae9c
Rishi Mondal <146999057+MAVRICK-1@users.noreply.github.com>
2024-02-04 05:42:29 +0000

    Added Firebase email and google auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app'
+import { initializeApp } from "firebase/app"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
