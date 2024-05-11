f9b3ca26f3fe1e0ea42255845313a2e8e7bd2e2b
Sid-80 <siddharthbramhecha@gmail.com>
2024-05-11 15:12:01 +0530

    conflicts resolved

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -7,13 +7,3 @@ export const store = configureStore({
         authReducer
     }
 })
-  
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({
-    //Reducer is a function that takes an action and updates the previous state 
-    reducer: {
-        
-    }
-})
-  
