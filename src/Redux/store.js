9e6d3cbe040748fe97ca92644a3181d86e16fcf2
Sid-80 <siddharthbramhecha@gmail.com>
2024-05-11 12:03:01 +0530

    feat: redux provider

---
+++ src/Redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    //Reducer is a function that takes an action and updates the previous state 
+    reducer: {
+        
+    }
+})
+  
