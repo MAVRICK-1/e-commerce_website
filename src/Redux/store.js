e142e93324ad7d15949ca5c61cd987345589c862
Sid-80 <siddharthbramhecha@gmail.com>
2024-05-11 14:59:55 +0530

    feat: redux-auth-slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth-slice"
 
 export const store = configureStore({
     //Reducer is a function that takes an action and updates the previous state 
     reducer: {
-        
+        authReducer
     }
 })
   
