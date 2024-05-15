909a7acc9ca801d30b739e0250a12217543cdac7
Avdhut Pailwan <74597375+AvdhutPailwan@users.noreply.github.com>
2024-05-15 22:05:22 +0530

    Update App.js to resolve merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { getDatabase, onValue, ref } from 'firebase/database';
-import React, { createContext, useEffect, useState } from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Loader from './assets/images/loading.gif';
-import Footer from './components/footer/footer';
-import Header from './components/header/header';
-import About from './pages/About';
-import AddProductForm from './pages/AddProd';
-import DetailsPage from './pages/Details';
-import Home from './pages/Home/index';
-import Listing from './pages/Listing';
-import NotFound from './pages/NotFound';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Cart from './pages/cart';
-import Wishlist from './pages/wishList';
-import './responsive.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  getDatabase,
+  onValue,
+  ref
+} from "firebase/database";
+import React, { createContext, useEffect, useState } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Loader from "./assets/images/loading.gif";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import About from "./pages/About";
+import AddProductForm from "./pages/AddProd";
+import DetailsPage from "./pages/Details";
+import Home from "./pages/Home/index";
+import Listing from "./pages/Listing";
+import NotFound from "./pages/NotFound";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+import ResetPassword from "./pages/ResetPassword";
+import Cart from "./pages/cart";
+import Wishlist from "./pages/wishList";
+import "./responsive.css";
 
 // import data from './data';
-import { collection, doc, getDocs } from 'firebase/firestore';
-import MapComponent from './components/map/ITEMmap';
-import { db } from './firebase';
-import SellerForm from './pages/SellerRegistration';
+import { collection, doc, getDocs } from "firebase/firestore";
+import MapComponent from "./components/map/ITEMmap";
+import { db } from "./firebase";
+import SellerForm from "./pages/SellerRegistration";
 
 const MyContext = createContext();
 
@@ -53,8 +59,8 @@ function App() {
 
   const fetchCartProducts = async () => {
     try {
-      const cartRef = doc(db, 'carts', localStorage.getItem('uid'));
-      const productsCollectionRef = collection(cartRef, 'products');
+      const cartRef = doc(db, "carts", localStorage.getItem("uid"));
+      const productsCollectionRef = collection(cartRef, "products");
       const querySnapshot = await getDocs(productsCollectionRef);
       const products = [];
       querySnapshot.forEach((doc) => {
@@ -63,26 +69,23 @@ function App() {
       setCartItems(products);
       setCartCount(products.length); // Set the product count
     } catch (error) {
-      console.error('Error fetching cart products:', error);
+      console.error("Error fetching cart products:", error);
     }
   };
 
   const fetchWishlistProducts = async () => {
-    console.log('fetchWishlistProducts');
     try {
-      const wishlistRef = doc(db, 'wishlists', localStorage.getItem('uid'));
-      const productsCollectionRef = collection(wishlistRef, 'products');
+      const wishlistRef = doc(db, "wishlists", localStorage.getItem("uid"));
+      const productsCollectionRef = collection(wishlistRef, "products");
       const querySnapshot = await getDocs(productsCollectionRef);
-      console.log(querySnapshot);
       const products = [];
       querySnapshot.forEach((doc) => {
         products.push({ id: doc.id, ...doc.data() });
       });
-      console.log(products);
       setWishlistItems(products);
       setWishlistCount(products.length); // Set the product count
     } catch (error) {
-      console.error('Error fetching wishlist products:', error);
+      console.error("Error fetching wishlist products:", error);
     }
   };
 
@@ -90,16 +93,15 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          'https://mavrick-1.github.io/DataApi/data.json'
+          "https://mavrick-1.github.io/DataApi/data.json"
         );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        // //console.log("fetced data", data)
         setData(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -109,7 +111,7 @@ function App() {
   useEffect(() => {
     getData();
 
-    const is_Login = localStorage.getItem('isLogin');
+    const is_Login = localStorage.getItem("isLogin");
     setIsLogin(is_Login);
 
     setTimeout(() => {
@@ -124,7 +126,7 @@ function App() {
       const db = getDatabase();
 
       // Reference to the node or path you want to fetch data from
-      const dataRef = ref(db, localStorage.getItem('user'));
+      const dataRef = ref(db, localStorage.getItem("user"));
 
       // Fetch data from the specified path
       onValue(
@@ -132,14 +134,13 @@ function App() {
         (snapshot) => {
           const data = snapshot.val();
           setCartItems(data);
-          //console.log("Data fetched successfully:", data);
         },
         (error) => {
-          console.error('Error fetching data:', error);
+          console.error("Error fetching data:", error);
         }
       );
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
@@ -149,7 +150,7 @@ function App() {
       const db = getDatabase();
 
       // Reference to the node or path you want to fetch data from
-      const dataRef = ref(db, localStorage.getItem('user'));
+      const dataRef = ref(db, localStorage.getItem("user"));
 
       // Fetch data from the specified path
       onValue(
@@ -157,20 +158,19 @@ function App() {
         (snapshot) => {
           const data = snapshot.val();
           setCartItems(data);
-          //console.log("Data fetched successfully:", data);
         },
         (error) => {
-          console.error('Error fetching data:', error);
+          console.error("Error fetching data:", error);
         }
       );
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   const addToCart = async (item) => {
     try {
-      const user = localStorage.getItem('user');
+      const user = localStorage.getItem("user");
       // Initialize Firebase database with the provided database URL
       const db = getDatabase();
       const cartRef = ref(db, user);
@@ -178,9 +178,8 @@ function App() {
       const uniqueKey = user + item.id; // Modify as per your requirement
       // Add item to the cart in Firebase
       setCartItems([...cartItems, { ...item, quantity: 1 }]);
-      //console.log('Item added to cart successfully');
     } catch (error) {
-      console.error('Error adding item to cart:', error);
+      console.error("Error adding item to cart:", error);
     }
   };
 
@@ -194,12 +193,12 @@ function App() {
   };
 
   const signIn = () => {
-    const is_Login = localStorage.getItem('isLogin');
+    const is_Login = localStorage.getItem("isLogin");
     setIsLogin(is_Login);
   };
 
   const signOut = () => {
-    localStorage.removeItem('isLogin');
+    localStorage.removeItem("isLogin");
     setIsLogin(false);
   };
 
@@ -225,7 +224,7 @@ function App() {
     wishlistCount,
     setWishlistCount,
     fetchCartProducts,
-    fetchWishlistProducts
+    fetchWishlistProducts,
   };
 
   return data && data.productData ? (
@@ -267,6 +266,9 @@ function App() {
           {/* sign in , signup Protection */}
           {isLogin === null && (
             <Route exact={true} path="signIn" element={<SignIn />} />
+          )}
+          {isLogin === null && (
+            <Route exact={true} path="resetpassword" element={<ResetPassword />} />
           )}
           {isLogin === null && (
             <Route exact={true} path="signUp" element={<SignUp />} />
