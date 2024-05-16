be4409fb9b3f4cf9cb5c88ababef0b31795c0cb0
anwesha <ANWESHA@DESKTOP-ALJFO4P.com>
2024-05-15 22:16:53 -0400

    refac:conflict resolved

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,316 +1,3 @@
-// import React, { useState, useContext } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import './style.css';
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-// import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
-// import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
-// import { Button, Snackbar, Typography } from '@mui/material';
-// import {
-//   getAuth,
-//   signInWithEmailAndPassword,
-//   GoogleAuthProvider,
-//   signInWithPopup,
-//   sendPasswordResetEmail
-// } from 'firebase/auth';
-// import { app } from '../../firebase';
-// import Backdrop from '@mui/material/Backdrop';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import { MyContext } from '../../App';
-// import GoogleImg from '../../assets/images/google.png';
-// import useLoggedInUserEmail from '../../Hooks/useLoggedInUserEmail';
-// import { useDispatch } from 'react-redux';
-// import { logIn } from '../../Redux/auth-slice';
-// const auth = getAuth(app);
-// const googleProvider = new GoogleAuthProvider();
-//
-// const SignIn = () => {
-//   const [showPassword, setShowPassword] = useState(false);
-//   const [mssg, setmssg] = useState();
-//   const [showLoader, setShowLoader] = useState(false);
-//   const [formFields, setFormFields] = useState({
-//     email: '',
-//     password: ''
-//   });
-//   const [error, setError] = useState('');
-//   const context = useContext(MyContext);
-//   const history = useNavigate();
-//   const [snackbarOpen, setSnackbarOpen] = useState(false);
-//   const [loggedInUserEmail, setLoggedInUseEmail] = useLoggedInUserEmail(); //get_email hook
-//   const [isDisabled, setIsDisabled] = useState(true);
-//
-//   const [inputErrors, setInputErrors] = useState({
-//     email: '',
-//     password: ''
-//   });
-//
-//   const checkInputs = (email, password) => {
-//     if (email.trim() !== '' && password.trim() !== '') {
-//       setIsDisabled(false);
-//     } else {
-//       setIsDisabled(true);
-//     }
-//   };
-//
-//   const dispatch = useDispatch();
-//
-//   function replaceSpecialCharacters(inputString) {
-//     // Use a regular expression to replace special characters with underscore _
-//     const replacedString = inputString.replace(/[#$\[\].]/g, '_');
-//
-//     return replacedString;
-//   }
-//
-//   // Email validation function
-//   const validateEmail = (email) => {
-//     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-//   };
-//
-//   // Password validation function
-//   const validatePassword = (password) => {
-//     return password.length != 0;
-//   };
-//
-//   const onChangeField = (e) => {
-//     const name = e.target.name;
-//     const value = e.target.value;
-//
-//     let errors = { ...inputErrors };
-//
-//     // Validate email
-//     if (name === 'email') {
-//       errors.email = !validateEmail(value) ? 'Invalid email address' : '';
-//     }
-//
-//     // Validate password
-//     if (name === 'password') {
-//       errors.password = !validatePassword(value) ? 'Password is required' : '';
-//     }
-//
-//     setInputErrors(errors);
-//     setFormFields((prevFormFields) => ({
-//       ...prevFormFields,
-//       [name]: value
-//     }));
-//     checkInputs(formFields.email, formFields.password, value);
-//   };
-//
-//   const signIn = () => {
-//     setShowLoader(true);
-//     signInWithEmailAndPassword(auth, formFields.email, formFields.password)
-//       .then((userCredential) => {
-//         const user = userCredential.user;
-//         setShowLoader(false);
-//         setFormFields({
-//           email: '',
-//           password: ''
-//         });
-//         localStorage.setItem('isLogin', true);
-//         const udata = replaceSpecialCharacters(user.email);
-//         localStorage.setItem('user', udata);
-//         context.signIn();
-//         dispatch(logIn({ email: user.email }));
-//         setLoggedInUseEmail(user.email);
-//         localStorage.setItem('uid', userCredential.user.uid);
-//         localStorage.setItem('userImage', '');
-//         //console.log(loggedInUserEmail);
-//         history('/');
-//       })
-//       .catch((error) => {
-//         setShowLoader(false);
-//         setError(error.message);
-//       });
-//   };
-//
-//   const signInWithGoogle = () => {
-//     //console.log('hi sign in');
-//     setShowLoader(true);
-//     signInWithPopup(auth, googleProvider)
-//       .then((result) => {
-//         setShowLoader(false);
-//         localStorage.setItem('isLogin', true);
-//         const udata = replaceSpecialCharacters(result.user.email);
-//         localStorage.setItem('user', udata);
-// <<<<<<< HEAD
-//         localStorage.setItem('uname', result.user.displayName);
-//         localStorage.setItem('uemail', result.user.email);
-// =======
-// >>>>>>> upstream/dev
-//         localStorage.setItem('uid', result.user.uid);
-//         context.signIn();
-//         setLoggedInUseEmail(udata);
-//         localStorage.setItem('userImage', result.user.photoURL);
-//         //console.log(loggedInUserEmail);
-//         history('/');
-//       })
-//       .catch((error) => {
-//         setShowLoader(false);
-//         setError(error.message);
-//       });
-//   };
-//
-//   const forgotPassword = async () => {
-// <<<<<<< HEAD
-//     history('/resetpassword');
-// =======
-//   history("/resetpassword")
-// >>>>>>> upstream/dev
-//   };
-//   const handleCloseSnackbar = () => {
-//     setSnackbarOpen(false);
-//   };
-//
-//   return (
-//     <>
-//       <section className="signIn mb-5">
-//         <div className="breadcrumbWrapper">
-//           <div className="container-fluid">
-//             <ul className="breadcrumb breadcrumb2 mb-0">
-//               <li>
-//                 <Link to="/">Home</Link>
-//               </li>
-//               <li>Sign In</li>
-//             </ul>
-//           </div>
-//         </div>
-//
-//         <div className="loginWrapper">
-//           <div className="card shadow">
-//             <Backdrop
-//               sx={{ color: '#000', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-//               open={showLoader}
-//               className="formLoader"
-//             >
-//               <CircularProgress color="inherit" />
-//             </Backdrop>
-//
-//             <h3 className="text-center">Sign In</h3>
-//             <form className="mt-4">
-//               <div className="form-group mb-4 w-100">
-//                 <TextField
-//                   id="email"
-//                   type="email"
-//                   name="email"
-//                   placeholder="Email"
-//                   className="w-100 text-zinc-800"
-//                   onChange={onChangeField}
-//                   value={formFields.email}
-//                   autoComplete="email"
-//                   error={inputErrors.email}
-//                 />
-//                 {inputErrors.email && (
-//                   <Typography
-//                     variant="caption"
-//                     sx={{ color: 'red', padding: '5px' }}
-//                   >
-//                     {inputErrors.email}
-//                   </Typography>
-//                 )}
-//               </div>
-//               <div className="form-group mb-4 w-100">
-//                 <div className="position-relative">
-//                   <TextField
-//                     id="password"
-//                     type={showPassword === false ? 'password' : 'text'}
-//                     name="password"
-//                     placeholder="Password"
-//                     className="w-100"
-//                     onChange={onChangeField}
-//                     value={formFields.password}
-//                     autoComplete="current-password"
-//                     error={inputErrors.password}
-//                   />
-//                   <Button
-//                     className="icon"
-//                     onClick={() => setShowPassword(!showPassword)}
-//                   >
-//                     {showPassword === false ? (
-//                       <VisibilityOffOutlinedIcon />
-//                     ) : (
-//                       <VisibilityOutlinedIcon />
-//                     )}
-//                   </Button>
-//                   {inputErrors.password && (
-//                     <Typography
-//                       variant="caption"
-//                       sx={{ color: 'red', padding: '5px' }}
-//                     >
-//                       {inputErrors.password}
-//                     </Typography>
-//                   )}
-//                 </div>
-//               </div>
-//
-//               {error && (
-//                 <div className="alert alert-danger" role="alert">
-//                   Invalid Email or Password
-//                 </div>
-//               )}
-//
-//               <div className="form-group mt-3 mb-4 w-100 d-flex justify-content-end">
-// <<<<<<< HEAD
-//                 <Button
-//                   className="btn btn-link float-end"
-//                   onClick={forgotPassword}
-//                 >
-//                   Forgot Password?
-//                 </Button>
-//               </div>
-// =======
-//               <Button className="btn btn-link float-end" onClick={forgotPassword}>
-//                 Forgot Password?
-//               </Button>
-//             </div>
-// >>>>>>> upstream/dev
-//
-//               <div className="form-group mt-5 mb-4 w-100">
-//                 <Button
-//                   disabled={isDisabled}
-//                   className="btn btn-g btn-lg w-100"
-//                   onClick={signIn}
-//                 >
-//                   Sign In
-//                 </Button>
-//               </div>
-//
-//               <div className="form-group mt-5 mb-4 w-100 signInOr">
-//                 <p className="text-center">OR</p>
-//                 <Button
-//                   className="w-100"
-//                   variant="outlined"
-//                   onClick={signInWithGoogle}
-//                 >
-//                   <img src={GoogleImg} alt="Google Logo" /> Sign In with Google
-//                 </Button>
-//               </div>
-//
-// <<<<<<< HEAD
-// =======
-//
-//
-// >>>>>>> upstream/dev
-//               <p className="text-center">
-//                 Don't have an account?{' '}
-//                 <b>
-//                   <Link to="/signup">Sign Up</Link>
-//                 </b>
-//               </p>
-//             </form>
-//           </div>
-//         </div>
-//       </section>
-//       <Snackbar
-//         open={snackbarOpen}
-//         autoHideDuration={6000}
-//         onClose={handleCloseSnackbar}
-//         message={mssg}
-//       />
-//     </>
-//   );
-// };
-//
-// export default SignIn;
-
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './style.css';
@@ -444,14 +131,6 @@ const SignIn = () => {
         localStorage.setItem('isLogin', true);
         const udata = replaceSpecialCharacters(result.user.email);
         localStorage.setItem('user', udata);
-<<<<<<< HEAD
-=======
-
-        localStorage.setItem('uname', result.user.displayName);
-        localStorage.setItem('uemail', result.user.email);
-
-
->>>>>>> 3a8a5522ed5850f7a4393702a5e984fc72e28251
         localStorage.setItem('uid', result.user.uid);
         context.signIn();
         setLoggedInUseEmail(udata);
@@ -466,15 +145,7 @@ const SignIn = () => {
   };
 
   const forgotPassword = async () => {
-<<<<<<< HEAD
     history('/resetpassword');
-=======
-
-    history('/resetpassword');
-
-  
-
->>>>>>> 3a8a5522ed5850f7a4393702a5e984fc72e28251
   };
   const handleCloseSnackbar = () => {
     setSnackbarOpen(false);
@@ -568,26 +239,13 @@ const SignIn = () => {
               )}
 
               <div className="form-group mt-3 mb-4 w-100 d-flex justify-content-end">
-<<<<<<< HEAD
                 <Button
-=======
-
-             Forgot Password?
-              </Button>
-            </div>   <Button
->>>>>>> 3a8a5522ed5850f7a4393702a5e984fc72e28251
                   className="btn btn-link float-end"
                   onClick={forgotPassword}
                 >
                   Forgot Password?
                 </Button>
               </div>
-<<<<<<< HEAD
-=======
-
-              
-
->>>>>>> 3a8a5522ed5850f7a4393702a5e984fc72e28251
 
               <div className="form-group mt-5 mb-4 w-100">
                 <Button
@@ -610,10 +268,6 @@ const SignIn = () => {
                 </Button>
               </div>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3a8a5522ed5850f7a4393702a5e984fc72e28251
               <p className="text-center">
                 Don't have an account?{' '}
                 <b>
