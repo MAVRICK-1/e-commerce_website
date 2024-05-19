08433ded5d93b8124c7f031474fd9fd92939d6ac
Hemu21 <rajva5727@gmail.com>
2024-05-19 20:21:43 +0530

    BTT

--- src/components/GoToTop/GoToTop.js
+++ src/components/GoToTop/GoToTop.js
@@ -4,23 +4,27 @@ import './GoToTop.css';
 
 const GoToTop = () => {
   const [isVisible, setIsVisible] = useState(false);
+
   const gotoBtn = () => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   };
 
   const listentoscroll = () => {
-    let heighttohidden = 250;
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop;
+    const heightToShow = 150;
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 
-    if (winScroll >= heighttohidden) {
+    if (winScroll >= heightToShow) {
       setIsVisible(true);
     } else {
       setIsVisible(false);
     }
   };
+
   useEffect(() => {
     window.addEventListener('scroll', listentoscroll);
+    return () => {
+      window.removeEventListener('scroll', listentoscroll);
+    };
   }, []);
 
   return (
